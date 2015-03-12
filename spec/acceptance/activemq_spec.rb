2b0a6def6162ce2998c612b4032846f4ee2c38fe
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-03-12 09:47:27 -0700

    More testing stuff

---
+++ spec/acceptance/activemq_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'activemq module' do
+  let(:manifest) {
+    <<-EOS
+      class { 'activemq': }
+    EOS
+  }
+
+  describe 'running puppet code' do
+    it 'should work with no errors' do
+      # Run it twice and test for idempotency
+      apply_manifest(manifest, :catch_failures => true)
+      expect(apply_manifest(manifest, :catch_changes => true).exit_code).to be_zero
+    end
+  end
+end
